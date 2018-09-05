53ac38d61c1e6ab589f444ebcccf4cdcf0b2931f
lucas.pereira <lucassporte_12@hotmail.com>
2018-09-05 07:41:12 -0300

    Estudo

---
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -0,0 +1,27 @@
+package br.com.devdojo.awesome.endpoint;
+
+import br.com.devdojo.awesome.model.Student;
+import br.com.devdojo.awesome.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@RestController
+@RequestMapping("student")
+public class StudentEndpoint {
+
+    @Autowired
+    private DateUtil dateUtil;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/list")
+    public List<Student> listAll(){
+//        System.out.println("Data e hora"+dateUtil.formatLocalDatTimeToDatabaseStyle(LocalDateTime.now()));
+        return asList(new Student("deku"), new Student("Todoroki"));
+    }
+}
