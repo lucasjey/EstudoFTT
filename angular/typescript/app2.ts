88db316ae706160beeacc3fa02e8ce1b07d14bf8
Isaias Batista <isaiasbatista96@gmail.com>
2018-09-14 11:58:43 -0300

    Classe pessoa arrumar

--- angular/typescript/app2.ts
+++ angular/typescript/app2.ts
@@ -1,6 +1,90 @@
 class Carro {
-    private modelo: string
-    private numeroDePortas: number
-    private velocidade: number = 0
-  
+  private modelo: string 
+  private numeroDePortas: number
+  private velocidade: number = 0
+
+  constructor(modelo: string, numeroDePortas :number){
+      this.modelo = modelo
+      this.numeroDePortas = numeroDePortas
+  }
+
+  public acelerar(): void{
+      this.velocidade = this.velocidade +10
+  }
+  public parar(): void{
+      this.velocidade = 0
+  }
+
+  public velocidadeAtual(): number{
+      return this.velocidade
+  }
 }
+
+class Concessionaria{
+  private endereco: string 
+  private listaDeCarros: Carro[]
+
+  constructor(endereco: string, listaDeCarros: Carro[]){
+    this.endereco = endereco
+    this.listaDeCarros = listaDeCarros
+  }
+
+  public fornecerEndereco(): string{
+    return this.endereco
+  }
+
+  public mostrarListaDeCarros(): Carro[]{
+    return this.listaDeCarros
+  }
+}
+
+class Pessoa {
+  private nome: string
+  private carroPreferido: string
+  private carro: Carro
+
+  constructor(nome: string, carroPreferido: string){
+    this.nome = nome
+    this.carroPreferido = carroPreferido
+    
+  }
+
+  public dizerNome(): string{
+      return this.nome
+  }
+
+  dizerCarroPreferido(): string{
+      return this.carroPreferido
+  }
+
+  comprarCarro(carro: Carro): void{
+      this.carro = carro
+  }
+
+  dizerCarroQueTem(): any{
+    return this.carro
+  }
+}
+
+
+/*criar carros*/
+let carroA = new Carro('dodge', 4)
+let carroB = new Carro('veloster', 3)
+let carroC = new Carro('saveiro', 2)
+
+/* lista de carros da concessionaria*/
+let listaDeCarros: Carro[] = [carroA, carroB, carroC]
+
+let concessionaria = new Concessionaria('Av. goias', listaDeCarros)
+
+/*exibir lista de carros*/
+// console.log(concessionaria.mostrarListaDeCarros())
+
+/*comprar carro*/
+let cliente = new Pessoa('jose', 'veloster')
+
+
+concessionaria.mostrarListaDeCarros().map((carro: Carro) => {
+  if(carro['modelo'] == cliente.dizerCarroPreferido())
+  carro['modelo'] == cliente.dizerCarroPreferido() ? cliente.comprarCarro(carro)
+})
--- angular/typescript/dist/app2.js
+++ angular/typescript/dist/app2.js
@@ -1,7 +1,67 @@
 "use strict";
 var Carro = /** @class */ (function () {
-    function Carro() {
+    function Carro(modelo, numeroDePortas) {
         this.velocidade = 0;
+        this.modelo = modelo;
+        this.numeroDePortas = numeroDePortas;
     }
+    Carro.prototype.acelerar = function () {
+        this.velocidade = this.velocidade + 10;
+    };
+    Carro.prototype.parar = function () {
+        this.velocidade = 0;
+    };
+    Carro.prototype.velocidadeAtual = function () {
+        return this.velocidade;
+    };
     return Carro;
 }());
+var Concessionaria = /** @class */ (function () {
+    function Concessionaria(endereco, listaDeCarros) {
+        this.endereco = endereco;
+        this.listaDeCarros = listaDeCarros;
+    }
+    Concessionaria.prototype.fornecerEndereco = function () {
+        return this.endereco;
+    };
+    Concessionaria.prototype.mostrarListaDeCarros = function () {
+        return this.listaDeCarros;
+    };
+    return Concessionaria;
+}());
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome, carroPreferido) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+    Pessoa.prototype.dizerNome = function () {
+        return this.nome;
+    };
+    Pessoa.prototype.dizerCarroPreferido = function () {
+        return this.carroPreferido;
+    };
+    Pessoa.prototype.comprarCarro = function (carro) {
+        this.carro = carro;
+    };
+    Pessoa.prototype.dizerCarroQueTem = function () {
+        return this.carro;
+    };
+    return Pessoa;
+}());
+/*criar carros*/
+var carroA = new Carro('dodge', 4);
+var carroB = new Carro('veloster', 3);
+var carroC = new Carro('saveiro', 2);
+/* lista de carros da concessionaria*/
+var listaDeCarros = [carroA, carroB, carroC];
+var concessionaria = new Concessionaria('Av. goias', listaDeCarros);
+/*exibir lista de carros*/
+// console.log(concessionaria.mostrarListaDeCarros())
+/*comprar carro*/
+var cliente = new Pessoa('jose', 'veloster');
+concessionaria.mostrarListaDeCarros().map(function (carro) {
+    if (carro['modelo'] == cliente.dizerCarroPreferido())
+        carro['modelo'] == cliente.dizerCarroPreferido() ? cliente.comprarCarro(carro)
+            :
+        ;
+});
